e4ffb9fbb2b198581603441ff9a0d098d6903fdb
micaelagarciaserra <micaelagarciaserra97@gmail.com>
2022-05-08 17:14:38 -0300

    hola

---
+++ semi.py
@@ -0,0 +1,5 @@
+print("Esto es una prueba")
+print("Intentando el primer commit/push ")
+variable="Esto es una varible del tipo string"
+variable2=2 #Esto es una variable del tipo int 
+variable3=2.0 #Esto es una variable del tipo float 
